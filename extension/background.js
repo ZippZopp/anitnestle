81248da334b41aed6ea9fbc42d901778daa4895e
ZippZopp <bene.schaeffler99@gmail.com>
2022-06-20 15:16:30 +0200

    updatet to mv3 and icon will change if product is from nestle

--- extension/background.js
+++ extension/background.js
@@ -1,2 +1,16 @@
+
+
 console.log('background running');
 
+
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(request.nestle)
+    if (request.nestle === true){
+      chrome.action.setIcon({path: 'nestle.png'});
+    }else{
+      chrome.action.setIcon({path: 'nonestle.png'});
+    }
+  }
+);
