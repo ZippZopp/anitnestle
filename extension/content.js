b8dbdd92fa0ab987edc5c9f38033ddded159fbea
ZippZopp <bene.schaeffler99@gmail.com>
2022-07-01 16:21:38 +0200

    improved company name comparison

--- extension/content.js
+++ extension/content.js
@@ -1,50 +1,50 @@
 $(document).ready(function () {
 
-  let brand = document.querySelectorAll('.a-spacing-small.po-brand .a-span9 .a-size-base')[0]
+  let brand_element = document.querySelectorAll('.a-spacing-small.po-brand .a-span9 .a-size-base')[0]
   
-  if(is_from_nestle(brand.innerText)){
-    warn_user(brand)
+  if(is_from_nestle(brand_element.innerText)){
+    warn_user(brand_element)
   }else{
-    mark_company_as_legit(brand)
+    mark_company_as_legit(brand_element)
   }
 
 });
 
-
 function is_from_nestle(name){
   let all_nestle_companies = ["Nestlé","Cerelac","Gerber","NaturNes","Acqua Panna","Perrier","S.Pellegrino", "San Pellegrino","Cheerios","Fitness","Lion","Nesquik Cereal","Aero","KitKat","Milkybar","Nestlé Les Recettes de l'Atelier","Quality Street","Smarties and a selection of our local confectionery brands","Blue Bottle Coffee","Nescafé","Nescafé Dolce Gusto","Nespresso","Starbucks Coffee At Home","Buitoni","Hot Pockets","Lean Cuisine","Maggi","Stouffer's","Thomy","Carnation","Coffee-Mate","La Laitière","Nido","Milo","Nesquik","Nestea","Chef","Chef-Mate","Maggi","Milo","Minor’s","Nescafé","Nestea","Sjora","Lean Cuisine","Stouffer's","Dreyer’s","Extrême","Häagen-Dazs","Mövenpick","Nestlé Ice Cream","Alpo","Bakers Complete","Beneful","Cat Chow","Dog Chow","Fancy Feast","Felix","Friskies","Gourmet","Purina","Purina ONE","Pro Plan"]
+  company_is_from_nestle = false
 
-  for(n_c in all_nestle_companies){
-    c = all_nestle_companies[n_c];
-    if(c.toLowerCase().replace("é", "e") ===  name.toLowerCase().replace("é", "e")){
-      return true
-    } 
-    /* 
-    if(accordance === true){
-      console.log(c +" = "+ name)
-    }else{
-      console.log(c +" / "+ name)
-    }*/
-  }
-  return false
+  all_nestle_companies.forEach(function(c_company) {
+    name.trim().split(/\s+/).forEach(function(single_name) {
+
+      //console.log(c_company +" ?= "+ single_name)
+      if(c_company.toLowerCase().replace("é", "e") ===  single_name.toLowerCase().replace("é", "e")){
+        //console.log("same")
+        company_is_from_nestle = true
+      }
+
+    });
+  });
+
+  return company_is_from_nestle
 }
 
-function warn_user(brand){
-  console.log(brand.innerText+" is from Nestlé")
-  /*if(brand.innerText !==  "Nestlé"){
-    brand.innerText = brand.innerText + " is from Nestlé"
+function warn_user(brand_element){
+  console.log(brand_element.innerText+" is from Nestlé")
+  /*if(brand_element.innerText !==  "Nestlé"){
+    brand_element.innerText = brand_element.innerText + " is from Nestlé"
   }*/
 
-  brand.style.color = "#FF0000"
-  insert_logo(brand,is_from_nestle=true)
+  brand_element.style.color = "#FF0000"
+  insert_logo(brand_element,is_from_nestle=true)
   inform_backgroundscirpt(is_from_nestle=true)
 }
 
-function mark_company_as_legit(brand){
-  console.log(brand.innerText+" is NOT from Nestlé")
+function mark_company_as_legit(brand_element){
+  console.log(brand_element.innerText+" is NOT from Nestlé")
   
-  //brand.style.color = "#00D100"
-  insert_logo(brand,is_from_nestle=false)
+  //brand_element.style.color = "#00D100"
+  insert_logo(brand_element,is_from_nestle=false)
   inform_backgroundscirpt(is_from_nestle=false)
 }
 
@@ -52,7 +52,7 @@ function inform_backgroundscirpt(is_from_nestle){
   chrome.runtime.sendMessage({nestle: is_from_nestle})
 }
 
-function insert_logo(brand,is_from_nestle){
+function insert_logo(brand_element,is_from_nestle){
   var img = document.createElement("img");
 
   if(is_from_nestle === true){
@@ -62,15 +62,15 @@ function insert_logo(brand,is_from_nestle){
   }
   
   img.style.height = "1em";
-  brand.after(img);
+  brand_element.after(img);
 
-  display_info_on_hover(brand,img,is_from_nestle=is_from_nestle)
+  display_info_on_hover(brand_element,img,is_from_nestle=is_from_nestle)
 
 }
 
-function display_info_on_hover(brand,img,is_from_nestle){
+function display_info_on_hover(brand_element,img,is_from_nestle){
   const newDiv = document.createElement("div");
-  newDiv.innerText = create_suitable_infotext(brand,is_from_nestle)
+  newDiv.innerText = create_suitable_infotext(brand_element,is_from_nestle)
   newDiv.style.position = "absolute"
   newDiv.style.visibility = "hidden"
   newDiv.style.letterSpacing = "1px"
@@ -98,11 +98,11 @@ function display_info_on_hover(brand,img,is_from_nestle){
 
 }
 
-function create_suitable_infotext(brand,is_from_nestle){
+function create_suitable_infotext(brand_element,is_from_nestle){
   if(is_from_nestle === true){
-    return brand.innerText + " is related to Nestlé"
+    return brand_element.innerText + " is related to Nestlé"
   }else{
-    return brand.innerText + " is NOT related to Nestlé"
+    return brand_element.innerText + " is NOT related to Nestlé"
   }
 }
 
